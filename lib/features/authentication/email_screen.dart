0fb8703ad8ac0457fa9045ff1ef31205c2288e6a
kangsudal_notebook <kangsudal@gmail.com>
2023-02-09 20:11:58 +0900

    EmailScreen1
    _isEmailValid() 추가
    키보드 사라지게 만들기(FocusScope.of(context).unfocus())

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -11,7 +11,7 @@ class EmailScreen extends StatefulWidget {
 }
 
 class _EmailScreenState extends State<EmailScreen> {
-  String _username = "";
+  String _email = "";
   final TextEditingController _usernameController = TextEditingController();
 
   @override
@@ -19,7 +19,7 @@ class _EmailScreenState extends State<EmailScreen> {
     super.initState();
     _usernameController.addListener(() {
       setState(() {
-        _username = _usernameController.text;
+        _email = _usernameController.text;
       });
     });
   }
@@ -30,52 +30,73 @@ class _EmailScreenState extends State<EmailScreen> {
     super.dispose();
   }
 
+  String? _isEmailValid() {
+    if (_email.isEmpty) return null;
+    final regExp = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+    );
+    if (regExp.hasMatch(_email)) {
+      return "Email not valid.";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'SignUp',
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'SignUp',
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size20,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v40,
-            Text(
-              'What is your email?',
-              style: TextStyle(
-                fontSize: Sizes.size24,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Gaps.v16,
-            TextField(
-              controller: _usernameController,
-              cursorColor: Theme.of(context).primaryColor,
-              decoration: InputDecoration(
-                hintText: "Email",
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade400,
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade400,
-                  ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              Text(
+                'What is your email?',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
-            ),
-            Gaps.v16,
-            FormButton(disabled: _username.isEmpty),
-          ],
+              Gaps.v16,
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                controller: _usernameController,
+                cursorColor: Theme.of(context).primaryColor,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  errorText: _isEmailValid(),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+              ),
+              Gaps.v16,
+              FormButton(disabled: _email.isEmpty || _isEmailValid != null),//에러가있으면 disabled가 true로 넘겨진다
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _onScaffoldTap() {
+    //focus된것을 모두 unfocus해준다 = 키보드가 사라진다
+    FocusScope.of(context).unfocus();
   }
 }
