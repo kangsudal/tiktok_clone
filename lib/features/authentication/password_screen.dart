99174429a95f6be5ce24d312b77ad50af3ed7ec9
kangsudal_notebook <kangsudal@gmail.com>
2023-02-11 10:52:09 +0900

    suffix 아이콘 추가

--- lib/features/authentication/password_screen.dart
+++ lib/features/authentication/password_screen.dart
@@ -1,10 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-class PasswordScreen extends StatelessWidget {
+class PasswordScreen extends StatefulWidget {
   const PasswordScreen({Key? key}) : super(key: key);
 
   @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  String _password = "";
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController.addListener(() {
+      setState(() {
+        _password = _passwordController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  String? _isPasswordValid() {
+    if (_password.isEmpty) return null;
+    final regExp = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+    );
+    if (!regExp.hasMatch(_password)) {
+      return "Email not valid.";
+    }
+    return null;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'SignUp',
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              Text(
+                'Password',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onEditingComplete: _onSubmit,
+                controller: _passwordController,
+                cursorColor: Theme.of(context).primaryColor,
+                decoration: InputDecoration(
+                  suffix: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidCircleXmark,
+                        color: Colors.grey.shade500,
+                        size: Sizes.size20,
+                      ),
+                      Gaps.h16,
+                      FaIcon(
+                        FontAwesomeIcons.eyeSlash,
+                        color: Colors.grey.shade500,
+                        size: Sizes.size20,//05:56
+                      )
+                    ],
+                  ),
+                  hintText: "Make it strong!",
+                  errorText: _isPasswordValid(),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+              ),
+              Gaps.v16,
+              GestureDetector(
+                onTap: _onSubmit,
+                child: FormButton(
+                  disabled: _password.isEmpty || _isPasswordValid() != null,
+                ),
+              ), //에러가있으면 disabled가 true로 넘겨진다
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onScaffoldTap() {
+    //focus된것을 모두 unfocus해준다 = 키보드가 사라진다
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (_isPasswordValid() != null || _password.isEmpty)
+      return; //이메일이 유효하지 않거나 비었으면 아무데도 안간다.
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => PasswordScreen(),
+      ),
+    );
   }
 }
