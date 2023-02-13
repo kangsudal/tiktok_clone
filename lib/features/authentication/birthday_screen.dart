aab7c24ed353c34f2a650ed81144ed0efd421b25
kangsudal_notebook <kangsudal@gmail.com>
2023-02-13 13:08:13 +0900

    Birthday Screen2:
    CupertinoDatePicker로 TextField 값 설정

--- lib/features/authentication/birthday_screen.dart
+++ lib/features/authentication/birthday_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({Key? key}) : super(key: key);
@@ -12,21 +14,25 @@ class BirthdayScreen extends StatefulWidget {
 }
 
 class _BirthdayScreenState extends State<BirthdayScreen> {
-  DateTime date = DateTime.now();
+  DateTime initialDate = DateTime.now();
   final TextEditingController _birthdayController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    final textDate = date.toString().split(' ').first;
-    _birthdayController.value =
-        TextEditingValue(text: textDate); //controller로 TextField의 값 설정
+    _setTextFieldDate(initialDate);
   }
 
   @override
   void dispose() {
     _birthdayController.dispose();
     super.dispose();
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = date.toString().split(' ').first;
+    _birthdayController.value =
+        TextEditingValue(text: textDate); //controller로 TextField의 값 설정
   }
 
   @override
@@ -86,13 +92,25 @@ class _BirthdayScreenState extends State<BirthdayScreen> {
           ],
         ),
       ),
+      //위젯을 하단에 고정시키는 법
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 300,
+          child: CupertinoDatePicker(
+            maximumDate: initialDate,
+            initialDateTime: initialDate,
+            mode: CupertinoDatePickerMode.date,//시간선택을 제외시키고 날짜선택만 되도록
+            onDateTimeChanged: _setTextFieldDate,
+          ),
+        ),
+      ),
     );
   }
 
   void _onNextTap() {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (_) => EmailScreen(),
+        builder: (_) => InterestsScreen(),
       ),
     );
   }
