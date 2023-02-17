4ca771661ecefc744509ab34676475e7888a0fa9
kangsudal_notebook <kangsudal@gmail.com>
2023-02-17 17:07:19 +0900

    AnimatedCrossFade

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { first, second }
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({Key? key}) : super(key: key);
@@ -10,112 +15,114 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showingPage = Page.first;
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
       child: Scaffold(
         body: SafeArea(
-          child: TabBarView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: Sizes.size24,
-                  right: Sizes.size24,
-                  bottom: Sizes.size16,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v52,
-                    Text(
-                      'Watch cool videos!',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      'Videos are personalized for you based on wat you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
+            child: AnimatedCrossFade(
+              firstChild: PageWidget(
+                title: 'Watch cool videos!',
+                subtitle:
+                    'Videos are personalized for you based on wat you watch, like, and share.',
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: Sizes.size24,
-                  right: Sizes.size24,
-                  bottom: Sizes.size16,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v52,
-                    Text(
-                      'Follow the rules!',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      'Videos are personalized for you based on wat you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
+              secondChild: PageWidget(
+                title: 'Follow the rules',
+                subtitle: '나이아가라폭포',
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: Sizes.size24,
-                  right: Sizes.size24,
-                  bottom: Sizes.size16,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v52,
-                    Text(
-                      'Enjoy the ride',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      'Videos are personalized for you based on wat you watch, like, and share.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              crossFadeState: _showingPage == Page.first
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond, //뭐 먼저 보여줄지
+              duration: Duration(milliseconds: 300),
+            ),
           ),
         ),
         bottomNavigationBar: BottomAppBar(
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              vertical: Sizes.size48,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TabPageSelector(selectedColor: Colors.black38,),
-              ],
-            ),
-          ),
+          child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: Sizes.size24,
+                vertical: Sizes.size24,
+              ),
+              child: AnimatedOpacity(
+                duration: Duration(milliseconds: 300),
+                opacity: _showingPage==Page.first?0:1,
+                child: CupertinoButton(
+                  child: Text('Enter the app'),
+                  onPressed: () {},
+                  color: Theme.of(context).primaryColor,
+                ),
+              )),
         ),
       ),
+    );
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    //사용자가 스와이프 하는 방향을 알게해주는 메서드
+    if (details.delta.dx > 0) {
+      //to the right
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      //to the left
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showingPage = Page.second;
+      });
+    } else {
+      setState(() {
+        _showingPage = Page.first;
+      });
+    }
+  }
+}
+
+class PageWidget extends StatelessWidget {
+  PageWidget({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  String title;
+  String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Gaps.v52,
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: Sizes.size40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Gaps.v16,
+        Text(
+          subtitle,
+          style: TextStyle(
+            fontSize: Sizes.size20,
+          ),
+        ),
+      ],
     );
   }
 }
