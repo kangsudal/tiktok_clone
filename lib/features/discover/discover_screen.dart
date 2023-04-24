7163effcf715fbf949f48420e6e2d615ab567e93
kangsudal_notebook <kangsudal@gmail.com>
2023-04-24 22:37:59 +0900

    ConstrainedBox

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -53,11 +53,16 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
       child: Scaffold(
         resizeToAvoidBottomInset: false, //텍스트필드를 선택했을때 아래 공백이 생기는것을 막아준다.
         appBar: AppBar(
-          title: MyCupertinoSearchTextField(
-            // CupertinoSearchTextField(
-            controller: _controller,
-            onChanged: _onSearchChanged, //글자가 바뀔때마다
-            onSubmitted: _onSearchSubmitted, //검색 버튼을 눌렀을때
+          title: ConstrainedBox(
+            //Container도 constraints parameter를 가진다. 대체해서 쓸 수 있다.
+            //검색박스가 더 커지지 않는다.
+            constraints: BoxConstraints(maxWidth: Breakpoints.sm),
+            child: MyCupertinoSearchTextField(
+              // CupertinoSearchTextField(
+              controller: _controller,
+              onChanged: _onSearchChanged, //글자가 바뀔때마다
+              onSubmitted: _onSearchSubmitted, //검색 버튼을 눌렀을때
+            ),
           ), //ios 스타일의 검색 텍스트 필드
           elevation: 1, //AppBar에 실선 넣기
           bottom: TabBar(
@@ -96,79 +101,80 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                 childAspectRatio: 9 / 20,
                 //Column의 내부 요소인 AspectRation(child:Fade...)와 Gaps와 Text를 아우를 수 있을만큼 높이가 길어져야한다.
               ),
-              itemBuilder: (context, idx) =>
-                  LayoutBuilder(builder: (context, constraints) {
-                return Column(
-                  children: [
-                    Container(
-                      clipBehavior: Clip.hardEdge,
-                      //이미지가 Container를 overflow해주기때문에 BoxDecoration의 BorderRadius가 적용이안된다. 그래서 Clip.hardEdge가 필요하다.
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(Sizes.size6),
-                      ),
-                      child: AspectRatio(
-                        aspectRatio: 9 / 16,
-                        child: FadeInImage.assetNetwork(
-                          //이미지를 불러오기전에 placeholder 이미지를 보여주고 FadeIn해준다.
-                          fit: BoxFit.cover,
-                          placeholder: 'assets/images/placeholder.jpg',
-                          image:
-                              'https://images.unsplash.com/photo-1679335649136-bd9db9e89bef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80',
+              itemBuilder: (context, idx) => LayoutBuilder(
+                builder: (context, constraints) {
+                  return Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        //이미지가 Container를 overflow해주기때문에 BoxDecoration의 BorderRadius가 적용이안된다. 그래서 Clip.hardEdge가 필요하다.
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.size6),
+                        ),
+                        child: AspectRatio(
+                          aspectRatio: 9 / 16,
+                          child: FadeInImage.assetNetwork(
+                            //이미지를 불러오기전에 placeholder 이미지를 보여주고 FadeIn해준다.
+                            fit: BoxFit.cover,
+                            placeholder: 'assets/images/placeholder.jpg',
+                            image:
+                                'https://images.unsplash.com/photo-1679335649136-bd9db9e89bef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80',
+                          ),
                         ),
                       ),
-                    ),
-                    Gaps.v10,
-                    Text(
-                      '${constraints.maxWidth.toInt()} This is very very very very very very long #long #long',
-                      style: TextStyle(
-                        fontSize: Sizes.size16,
-                        fontWeight: FontWeight.bold,
-                        overflow: TextOverflow.ellipsis, //말줄임표
-                      ),
-                      maxLines: 2, //2줄로 제한해줘야 overflow를 막을 수 있다.
-                    ),
-                    Gaps.v10,
-                    if (constraints.maxWidth < 200 ||
-                        constraints.maxWidth > 250)
-                      DefaultTextStyle(
-                        //위젯 안에 들어있는 모든 TextStyle이 같게 된다.
+                      Gaps.v10,
+                      Text(
+                        '${constraints.maxWidth.toInt()} This is very very very very very very long #long #long',
                         style: TextStyle(
+                          fontSize: Sizes.size16,
                           fontWeight: FontWeight.bold,
-                          color: Colors.grey,
+                          overflow: TextOverflow.ellipsis, //말줄임표
                         ),
-                        child: Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 12, //13부턴 AspectRatio사이즈를 넘어선다.
-                            ),
-                            Gaps.h4,
-                            Expanded(
-                              //Row에서 정해진 가로 너비가 없어서(Column에서 무한한 높이) overflow가 나서 Expanded를 해준다.
-                              child: Text(
-                                'Very Very Very long nickname',
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1, //1줄로 제한
-                                style: TextStyle(
-                                  fontSize: 12,
-                                ), //DefaultTextStyle 위에 덮어쓸 수 있다.
-                              ),
-                            ),
-                            Gaps.h4,
-                            FaIcon(
-                              FontAwesomeIcons.heart,
-                              size: Sizes.size20,
-                              color: Colors.grey.shade400,
-                            ),
-                            Gaps.h2,
-                            Text(
-                              "25K",
-                            ),
-                          ],
-                        ),
+                        maxLines: 2, //2줄로 제한해줘야 overflow를 막을 수 있다.
                       ),
-                  ],
-                );
-              }),
+                      Gaps.v10,
+                      if (constraints.maxWidth < 200 ||
+                          constraints.maxWidth > 250)
+                        DefaultTextStyle(
+                          //위젯 안에 들어있는 모든 TextStyle이 같게 된다.
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 12, //13부턴 AspectRatio사이즈를 넘어선다.
+                              ),
+                              Gaps.h4,
+                              Expanded(
+                                //Row에서 정해진 가로 너비가 없어서(Column에서 무한한 높이) overflow가 나서 Expanded를 해준다.
+                                child: Text(
+                                  'Very Very Very long nickname',
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1, //1줄로 제한
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ), //DefaultTextStyle 위에 덮어쓸 수 있다.
+                                ),
+                              ),
+                              Gaps.h4,
+                              FaIcon(
+                                FontAwesomeIcons.heart,
+                                size: Sizes.size20,
+                                color: Colors.grey.shade400,
+                              ),
+                              Gaps.h2,
+                              Text(
+                                "25K",
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                  );
+                },
+              ),
             ),
             for (var tab in tabs.skip(1))
               //skip(1): 첫번째 위젯으로 GridView가 와야하기때문에 Center(child:Text("top"))은 제외시킴.
